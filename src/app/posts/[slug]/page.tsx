db71717f76bc1c78d5646e2a9d88c5f5a4baea2f
michael <michael@michael.local>
2025-07-19 23:28:46 +0800

    add

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,20 +2,10 @@ import { processMarkdown } from '@/lib/markdown'
 import Layout from '@/components/Layout'
 import PostHeader from '@/components/PostHeader'
 import PostFooter from '@/components/PostFooter'
-// import TableOfContents from '@/components/TableOfContents'
 import { notFound } from 'next/navigation'
-import type { Metadata, ResolvingMetadata } from 'next'
 
 interface Props {
   params: Promise<{ slug?: string }>; // Updated to be a Promise
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug } = await params; // Await the params object
-  // ... rest of your generateMetadata logic
-  return {
-    title: `Blog Post: ${slug}`
-  }
 }
 
 async function getPost(slug: string) {
@@ -35,7 +25,7 @@ async function getPost(slug: string) {
 export default async function PostPage({ params }: Props) {
   const { slug = "" } = await params; // Await the params object
   const post = await getPost(slug)
-  // console.log('post', post)
+  console.log('post', post)
 
   if (!post) {
     notFound()
